01bf6baba15a03c8212ac6e707a8d7c68d70b93c
Emile <72444841+EmileJ@users.noreply.github.com>
2023-08-30 18:23:41 +0200

    Update JavaLong.py final

--- JavaLong.py
+++ JavaLong.py
@@ -15,15 +15,15 @@ class JavaLong:
         if not MIN_VAL <= result <= MAX_VAL:
             result %= MIN_VAL * 2
         return JavaLong(result)
-    
-    def __str__(self):
-        return str(self.value)
 
     def __repr__(self):
-        return int(self.value)
+        return str(self.value)
 
 def convert(strInput):
     j2 = JavaLong(0)
     for i in strInput:
         j2 = (j2 * JavaLong(31)) + JavaLong(ord(i))
         print(j2)
+    if(j2<0):
+        j2*=-1
+    return j2
