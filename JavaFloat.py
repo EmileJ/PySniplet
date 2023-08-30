4c9027afda1c1fb4ada803c678274338e1132f1f
Emile <72444841+EmileJ@users.noreply.github.com>
2023-08-30 18:11:55 +0200

    Update JavaFloat.py

--- JavaFloat.py
+++ JavaFloat.py
@@ -1,18 +1,20 @@
+MIN_VAL = -9223372036854775808
+MAX_VAL = 9223372036854775807
 class JavaLong:
     def __init__(self, value):
         self.value = value
 
     def __add__(self, other):
         result = self.value + other.value
-        if not -9223372036854775808 <= result <= 9223372036854775807:
-            result -= 9223372036854775808 * 2
-        return CustomInt(result)
+        if not MIN_VAL <= result <= MAX_VAL:
+            result -= MIN_VAL * -2
+        return JavaLong(result)
 
     def __mul__(self, other):
         result = self.value * other.value
-        if not -9223372036854775808 <= result <= 9223372036854775807:
-            result %= 9223372036854775808 * 2
-        return CustomInt(result)
+        if not MIN_VAL <= result <= MAX_VAL:
+            result %= MIN_VAL * -2
+        return JavaLong(result)
     
     def __str__(self):
         return str(self.value)
